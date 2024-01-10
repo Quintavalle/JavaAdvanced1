2cefb9a26b444f493e8a401f2a9d3a3429ba3fd1
Quintavalle <147600523+Quintavalle@users.noreply.github.com>
2024-01-10 19:17:48 +0100

    esercizio5M4

---
+++ esercizio5M4/src/Person.java
@@ -0,0 +1,96 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String address;
+
+    private Person(Builder builder) {
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.age = builder.age;
+        this.address = builder.address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        private int age;
+        private String address;
+
+        public Builder(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person.Builder("John", "Doe")
+                .age(30)
+                .address("123 Main Street")
+                .build();
+
+        Person person2 = new Person.Builder("Jane", "Smith")
+                .age(25)
+                .build();
+
+        System.out.println(person1);
+        System.out.println(person2);
+    }
+}
